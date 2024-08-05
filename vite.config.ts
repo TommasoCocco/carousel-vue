5b103e7f83a6b53eb2107d5316fa62c6c9886db0
TommasoG <tommasococco02@gmail.com>
2024-08-05 15:16:55 +0200

    set public dir

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/carousel-vue/',
+  publicDir: '/carousel-vue/',
   plugins: [
     vue(),
     vueJsx(),
